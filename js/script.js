0f096187f1a3d75484317b0c133f2df07b4aa19f
JF-ok <jen000@yandex.ru>
2020-04-03 15:39:30 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function (){
+ var links = document.querySelectorAll(".links_buttons div")
+ for (var i = 0; i < links.length; i++) {
+ 	links[i].onclick = function (){
+ 		if (this.getAttribute("state") == "open") {
+ 			this.setAttribute("state", "close")
+ 		}
+ 		else {
+ 			this.setAttribute("state", "open")
+ 		}
+ 	}
+ }
+}
